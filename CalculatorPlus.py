c90465d0d55092e39c4b495eb61560b747deb8f1
Pankaj <pankajgacche.sdet@gmail.com>
2024-04-27 11:22:15 +0530

    Modified code

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -2,37 +2,32 @@ import math
 
 class Calculator:
 
-def add(self, a, b):
+    def add(self, a, b):
+        return a + b
 
-return a + b
+    def subtract(self, a, b):
+        return a - b
 
-def subtract(self, a, b):
+    def multiply(self, a, b):
+        return a * b
+    
+    def divide(self, a, b):
+        return a / b
 
-return a - b
-
-def multiply(self, a, b):
-
-return a * b
-
-def divide(self, a, b):
-
-return a / b
-
-def square_root(self, x):
-
-return math.sqrt(x)
+    def square_root(self, x):
+        return math.sqrt(x)
 
 if __name__ == "__main__":
+ calculator = Calculator()
 
-calculator = Calculator()
-
-num1 = 16
-
+num1 = 16   
 num2 = 4
 
 print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
 
-print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}") print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
+print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}")
+
+print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
 
 print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
 
